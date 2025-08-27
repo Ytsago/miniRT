bb25a5aa4ac94b7ebc727a2d2e3c8e2c2a519334
Yazid Bokhari <yabokhar@z1r7p3.42lyon.fr>
2025-08-27 20:54:02 +0200

    Multiple lights

--- src/ray/lightning.c
+++ src/ray/lightning.c
@@ -6,7 +6,7 @@
 /*   By: yabokhar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/23 20:18:33 by yabokhar          #+#    #+#             */
-/*   Updated: 2025/08/23 21:17:10 by yabokhar         ###   ########.fr       */
+/*   Updated: 2025/08/27 20:42:35 by yabokhar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,29 +27,40 @@ static bool	in_shadow(const t_list *objs, t_ray ray, double max_dist);
 
 t_vect3	lightning(t_context *scene, t_point3 p, t_vect3 n, t_vect3 obj_color)
 {
-	const t_vect3	ambient = \
-	vect3_mult(vect3_const_mult(color_to_vec(scene->ambient_lightning.color), \
-	scene->ambient_lightning.ratio), obj_color);
-	const t_vect3	light_dir = \
-	vect3_unit(vect3_sub(scene->lights.light_point, p));
-	const double	light_dist = vect3_norm((double *)light_dir.coords);
+	t_vect3			total_lightning;
+	t_list			*light;
+	t_light			*current_light;
+	t_vect3			light_dir;
+	double			light_dist;
 	const t_vect3	light_color = color_to_vec(scene->lights.color);
 	t_vect3			v_reflections[2];
 
 	ft_fbzero(v_reflections, sizeof(t_vect3) * 2);
-	if (!in_shadow(scene->objects, \
-	(t_ray){vect3_add(p, vect3_const_mult(n, T_MIN)), light_dir}, light_dist))
+	total_lightning = vect3_mult(vect3_const_mult(color_to_vec(\
+	scene->ambient_lightning.color), \
+	scene->ambient_lightning.ratio), obj_color);
+	light = scene->lights_list;
+	while (light)
 	{
-		v_reflections[DIFF] = vect3_mult(vect3_const_mult(light_color, \
-		scene->lights.brightness_ratio * \
-		fmax(vect3_scalar(n, light_dir), 0.0)), obj_color);
-		v_reflections[SPEC] = vect3_const_mult(light_color, \
-		scene->lights.brightness_ratio * \
-		pow(fmax(vect3_scalar(n, vect3_unit(vect3_add(light_dir, \
-		vect3_unit(vect3_sub(scene->camera.view_point, p))))), 0.0), 12));
+		current_light = light->content;
+		light_dir = vect3_unit(vect3_sub(current_light->light_point, p));
+		light_dist = vect3_norm((double *)light_dir.coords); 
+		if (!in_shadow(scene->objects, \
+		(t_ray){vect3_add(p, vect3_const_mult(n, T_MIN)), light_dir}, light_dist))
+		{
+			v_reflections[DIFF] = vect3_mult(vect3_const_mult(light_color, \
+			scene->lights.brightness_ratio * \
+			fmax(vect3_scalar(n, light_dir), 0.0)), obj_color);
+			v_reflections[SPEC] = vect3_const_mult(light_color, \
+			scene->lights.brightness_ratio * \
+			pow(fmax(vect3_scalar(n, vect3_unit(vect3_add(light_dir, \
+			vect3_unit(vect3_sub(scene->camera.view_point, p))))), 0.0), 12));
+			total_lightning = vect3_add(total_lightning, v_reflections[DIFF]);
+			total_lightning = vect3_add(total_lightning, v_reflections[SPEC]);
+		}
+		light = light->next;
 	}
-	return (vect3_add(vect3_add(ambient, v_reflections[DIFF]), \
-	v_reflections[SPEC]));
+	return (total_lightning);
 }
 
 static bool	in_shadow(const t_list *objs, t_ray ray, double max_dist)
