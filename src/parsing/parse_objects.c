794031780e2fcaa4d4dc0725681ee00472c0d463
Sebastien Cros <secros@z1r7p5.42lyon.fr>
2025-09-02 14:29:00 +0200

    broken light

--- src/parsing/parse_objects.c
+++ src/parsing/parse_objects.c
@@ -6,7 +6,7 @@
 /*   By: secros <secros@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/06 11:29:39 by secros            #+#    #+#             */
-/*   Updated: 2025/09/02 11:16:08 by secros           ###   ########.fr       */
+/*   Updated: 2025/09/02 14:19:55 by secros           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,6 +33,8 @@ t_object	*new_plane(t_context *scene, char **line)
 	jump_spaces(line);
 	if (!get_color(scene, line, &new->color))
 		return (free_and_return_null(new));
+	// new->texture[0] = load_image(&scene->screen_ptr, "./test.xpm");
+	new->texture[1] = load_image(&scene->screen_ptr, "./test_normal.xpm");
 	return ((t_object *) new);
 }
 
--- src/ray/bumpmapping.c
+++ src/ray/bumpmapping.c
@@ -6,11 +6,15 @@
 /*   By: secros <secros@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/02 10:18:09 by secros            #+#    #+#             */
-/*   Updated: 2025/09/02 11:07:57 by secros           ###   ########.fr       */
+/*   Updated: 2025/09/02 14:20:53 by secros           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "miniRT.h"
+#include <math.h>
+
+#define U 0
+#define V 1
 
 t_vect3	get_normal_map(t_pict *map, double u, double v)
 {
@@ -39,6 +43,33 @@ t_vect3	sphere_mapping(t_object *obj, t_point3 p, t_vect3* normal)
 	return (color_to_vec(obj->color)); //duplicated in get_pixel_color
 }
 
+t_vect3	plane_mapping(t_object *obj, t_point3 p, t_vect3 *normal)
+{
+	t_vect3	axis[2];
+	t_vect3	to_point;
+	t_vect3	tmp;
+	double	u;
+	double	v;
+
+	tmp = (t_vect3) {{1, 0, 0}};
+	if (fabs(vect3_scalar(*normal, tmp)) > 0.9)
+		tmp = (t_vect3) {{0, 1, 0}};
+	axis[V] = vect3_unit(vect3_cross(*normal, tmp));
+	axis[U] = vect3_cross(*normal, axis[V]);
+	to_point = vect3_sub(p, obj->pos);
+	u = fmod(vect3_scalar(to_point, axis[U]) / 5, 1.0);
+	v = fmod(vect3_scalar(to_point, axis[V]) / 5, 1.0);
+	if (u < 0.0)
+		u += 1.0;
+	if (v < 0.0)
+		v += 1.0;
+	if (obj->texture[1])
+		*normal = get_normal_map(obj->texture[1], u, v);
+	if (obj->texture[0])
+		return (color_to_vec(coord_to_img(obj->texture[0], u, v)));
+	return (color_to_vec(obj->color));
+}
+
 t_color	get_pixel_color(t_object *obj, t_context *scene, \
 	t_point3 p, t_vect3 normal)
 {
@@ -49,5 +80,7 @@ t_color	get_pixel_color(t_object *obj, t_context *scene, \
 		return (vec_to_color(lightning(scene, p, normal, texture))); 
 	if (obj->type == SPHERE)
 		texture = sphere_mapping(obj, p, &normal);
+	if (obj->type == PLANE)
+		texture = plane_mapping(obj, p, &normal);
 	return (vec_to_color(lightning(scene, p, normal, texture)));
 }
